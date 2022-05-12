5c35ae6536aa8f94d96303d9d0a50103c1457ecb
RiazK <RiazCKhan@gmail.com>
2022-05-12 17:20:32 -0400

    Import State and axios call

--- react-front-end/src/components/ShopList.jsx
+++ react-front-end/src/components/ShopList.jsx
@@ -1,7 +1,20 @@
-export default function ShopList(){
+import { useState } from "react"
+
+export default function ShopList() {
+
+  const [shopData, setShopData] = useState([])
+
+  axios.get('/api/items')
+    .then((response, request) => {
+      console.log(response.data)
+    })
+
+
+// const shopItem = 
+
   return(
     <>
-    
+
     </>
   )
 }
