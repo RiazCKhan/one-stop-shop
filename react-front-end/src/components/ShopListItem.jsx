e8e2b3bbf55a85a73521113fa9986b2291937127
RiazK <RiazCKhan@gmail.com>
2022-05-12 17:17:47 -0400

    Create Skeleton

---
+++ react-front-end/src/components/ShopListItem.jsx
@@ -0,0 +1,14 @@
+export default function ShopListItem(){
+  return(
+    <>
+    <div>
+      <h1>Title</h1>
+      <h3>Image TBD</h3>
+      <ul>
+        <li>Description</li>
+        <li>Cost</li>
+      </ul>
+    </div>
+    </>
+  )
+}
