4cc4cc9ed8f5f37b572b4fa584b4cd084e5b7adf
RiazK <RiazCKhan@gmail.com>
2022-05-12 14:58:59 -0400

    create fake seed data

---
+++ express-back-end/database/seeds/01_items.sql
@@ -0,0 +1,8 @@
+INSERT INTO items (title, description, cost) VALUES ('Stump', 'Might be a good seat...', 15)
+INSERT INTO items (title, description, cost) VALUES ('Ace of Spades', 'Jackpot?', 48)
+INSERT INTO items (title, description, cost) VALUES ('Generic Water Bottle', 'Quench your thirst', 23)
+INSERT INTO items (title, description, cost) VALUES ('Desk Lamp', 'Are you afraid of the dark?', 99)
+INSERT INTO items (title, description, cost) VALUES ('Rubber Ducky', 'Best Friend', 1)
+INSERT INTO items (title, description, cost) VALUES ('Candy Wrapper', 'Timeless', 3)
+INSERT INTO items (title, description, cost) VALUES ('Lightning Rod', '+4 Agility', 18)
+INSERT INTO items (title, description, cost) VALUES ('Katana', '18 years and older', 77)
