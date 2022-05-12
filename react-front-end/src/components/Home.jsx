9e310529e97d08f1c6e2bf4dabdb7d7b6da4a980
RiazK <RiazCKhan@gmail.com>
2022-05-12 17:20:09 -0400

    remove axios call

--- react-front-end/src/components/Home.jsx
+++ react-front-end/src/components/Home.jsx
@@ -2,12 +2,6 @@ import axios from 'axios';
 
 export default function Home() {
 
-
-  axios.get('/api/items')
-    .then((response, request) => {
-      console.log(response.data)
-    })
-
   return (
     <>
       Hello
