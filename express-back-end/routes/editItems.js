a4e053241b71a06033d86dd5123c786d9384ce81
RiazK <RiazCKhan@gmail.com>
2022-05-13 12:34:55 -0400

    create query string, testing for req

--- express-back-end/routes/editItems.js
+++ express-back-end/routes/editItems.js
@@ -4,6 +4,13 @@ module.exports = (db) => {
 
  router.put("/editItems/", (req, res) => {
 
+  console.log('data on back end', req.body)
+
+  db.query(`
+  UPDATE items
+  SET cost = $1
+  WHERE items.id = $2
+  `, [])
 
 })
 
