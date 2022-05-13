b25440b5c30dbe12443543f6e0ebbda14f38978a
RiazK <RiazCKhan@gmail.com>
2022-05-13 10:12:48 -0400

    simple Nav layout

---
+++ react-front-end/src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+import "./NavBar.css"
+
+export default function NavBar() {
+  return(
+    <section className="nav-container">
+      <h1>One Stop Shop</h1>
+        <h3>Add Item</h3>
+    </section>
+  )
+}
