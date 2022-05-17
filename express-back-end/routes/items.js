ad25b1a840188c3656aecd051d02a23863f7f433
RiazK <RiazCKhan@gmail.com>
2022-05-17 18:23:52 -0400

    add notes, promise pending

--- express-back-end/routes/items.js
+++ express-back-end/routes/items.js
@@ -7,9 +7,25 @@ module.exports = db => {
         res.json(items);
       });
   })
-  
+
+  // Add Item
   router.post("/newItems/", (req, res) => {
-    console.log(req.body)
+    console.log('Receive Data from Frontend', req.body)
+
+    const itemName = req.body.itemName
+    const itemDescription = req.body.itemDescription
+    const itemCost = req.body.itemCost
+
+    db.query(`
+    INSERT INTO items (title, description, cost) 
+    VALUE ($1::VARCHAR, $2::VARCHAR, $3::INT)
+    `, [itemName, itemDescription, itemCost])
+      .then(() => {
+        res.status(204).json({})
+        console.log("Item Added")
+      }).catch((error) => {
+        console.log('Cost Update Error', error)
+      })
   })
 
   // Update Item Cost
