6accc65445c1196e03d35309d78e45f62c9e6e96
RiazK <RiazCKhan@gmail.com>
2022-05-17 17:05:08 -0400

    insert basic form

--- react-front-end/src/components/NewItem.jsx
+++ react-front-end/src/components/NewItem.jsx
@@ -1,11 +1,19 @@
+import "./NewItem.css";
+
 export default function NewItem() {
+  return (
 
+    <form>
+      <label for="item_name">Item Name</label>
+      <input type="text" placeholder="Item name.." />
 
-return (
-  <>
+      <label for="item_desc">Item Description</label>
+      <input type="text" placeholder="Item Description" />
 
-  </>
-)
+      <label for="item_cost">Item Cost</label>
+      <input type="number" placeholder="$$$" />
 
-
+      <input type="submit" value="Submit" />
+    </form>
+  )
 }
