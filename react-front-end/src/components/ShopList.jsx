893dc2c9c0e1e77bce156db7fc6253a682022c66
RiazK <RiazCKhan@gmail.com>
2022-05-17 19:24:31 -0400

    send item ID to backend for deletion

--- react-front-end/src/components/ShopList.jsx
+++ react-front-end/src/components/ShopList.jsx
@@ -47,11 +47,11 @@ export default function ShopList() {
   }
 
   const onDelete = (id) => {
-    console.log('click')
-    axios.delete('/api/deleteItem', id)
-      .then(() => setPage(true))
-      .then(() => setPage(false))
-      .catch((error) => console.log(error))
+    console.log(id)
+    axios.delete('/api/deleteItem', { data: { id }})
+      // .then(() => setPage(true))
+      // .then(() => setPage(false))
+      // .catch((error) => console.log(error))
   }
 
   const shopItem = shopData.map((item) => {
