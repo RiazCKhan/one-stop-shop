ba8c0c81251ac106455cfcac88111b3d07b9baf2
RiazK <RiazCKhan@gmail.com>
2022-05-17 16:21:14 -0400

    implement spread state update

--- react-front-end/src/components/ShopList.jsx
+++ react-front-end/src/components/ShopList.jsx
@@ -7,10 +7,12 @@ import "./ShopList.css"
 export default function ShopList() {
 
   const [shopData, setShopData] = useState([])
-  const [newCost, setNewCost] = useState("")
+  const [newCost, setNewCost] = useState({}) // set new cost to an object, indexed based on shop data id
   const [page, setPage] = useState(false)
 
+
   useEffect(() => {
+    console.log("[useEffect]: page " + page)
     axios.get('/api/items')
       .then((response) => {
         setShopData(response.data)
@@ -23,25 +25,38 @@ export default function ShopList() {
     event.preventDefault();
   }
 
+  console.log("new cost: ", newCost)
+
+  const newCostHandler = (id, newCost) => {
+    // let newCostCopy = { ...newCost }
+    let newCostCopy = Object.assign({}, newCost)
+    newCostCopy[id] = ""
+    return newCostCopy
+  }
+
   const onEdit = (id) => {
     const item = {
       id: id,
-      cost: newCost
+      cost: newCost[id]
     }
     axios.put('/api/editItems', item)
       .then(() => {
-        console.log("Sending Data to Backend, Item ID:", id)
-        setPage(true)
+        setPage(true);
+        // newCost[id] = "" // clear the new cost id
+        // setNewCost(newCost);
       })
       .then(() => {
-        setPage(false)
+        setNewCost(newCostHandler(id, newCost))
+        setPage(false);
       })
       .catch((error) => {
         console.log(error)
       })
+
   }
 
   const shopItem = shopData.map((item) => {
+    console.log("[shopData.map] " + newCost)
     return (
       <ShopListItem
         key={item.id}
@@ -49,6 +64,7 @@ export default function ShopList() {
         title={item.title}
         description={item.description}
         cost={item.cost}
+        newCost={newCost}
         onEdit={onEdit}
         setNewCost={setNewCost}
         handleSubmit={handleSubmit}
--- react-front-end/src/components/ShopListItem.jsx
+++ react-front-end/src/components/ShopListItem.jsx
@@ -4,6 +4,13 @@ export default function ShopListItem(props) {
 
   const { id, title, description, cost, onEdit, newCost, setNewCost, handleSubmit } = props;
 
+  const newCostHandler = (id, newCost, value) => {
+    // let newCostCopy = { ...newCost }
+    let newCostCopy = Object.assign({}, newCost)
+    newCostCopy[id] = value
+    return newCostCopy
+  }
+
   return (
     <article className="item-card">
       <h1>{title}</h1>
@@ -19,8 +26,10 @@ export default function ShopListItem(props) {
           <div className="edit-input">
             <input
               type="number"
-              value={newCost}
-              onChange={(event) => setNewCost(event.target.value)}
+              value={newCost[id] || ""}
+              onChange={(event) => {
+                setNewCost(newCostHandler(id, newCost, event.target.value))
+              }}
             />
             <button onClick={() => onEdit(id)}>Update Cost</button>
           </div>
