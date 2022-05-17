f11e9cb3d74aac0f594b122287f72bb10f0e06a7
RiazK <RiazCKhan@gmail.com>
2022-05-17 16:44:16 -0400

    small css nav edit

--- react-front-end/src/components/NavBar.jsx
+++ react-front-end/src/components/NavBar.jsx
@@ -1,10 +1,16 @@
+import { Link } from "react-router-dom";
 import "./NavBar.css"
 
 export default function NavBar() {
-  return(
+  return (
     <section className="nav-container">
+      <Link to="/">
+        <h3>All Items</h3>
+      </Link>
       <h1>One Stop Shop</h1>
+      <Link to="">
         <h3>Add Item</h3>
+      </Link>
     </section>
   )
 }
