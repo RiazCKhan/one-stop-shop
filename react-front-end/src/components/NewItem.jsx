3d527de892a7f327fc7e3988a81705f8e6b88844
RiazK <RiazCKhan@gmail.com>
2022-05-17 17:13:38 -0400

    update form input fields

--- react-front-end/src/components/NewItem.jsx
+++ react-front-end/src/components/NewItem.jsx
@@ -3,12 +3,12 @@ import "./NewItem.css";
 export default function NewItem() {
   return (
 
-    <form>
+    <form className="add-form-container">
       <label for="item_name">Item Name</label>
       <input type="text" placeholder="Item name.." />
 
       <label for="item_desc">Item Description</label>
-      <input type="text" placeholder="Item Description" />
+      <input type="textarea" placeholder="Item Description" />
 
       <label for="item_cost">Item Cost</label>
       <input type="number" placeholder="$$$" />
