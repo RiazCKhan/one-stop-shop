cbd2817aa1f8c749b0c374798f85c46d1fb949e9
RiazK <RiazCKhan@gmail.com>
2022-05-17 18:11:44 -0400

    remove unused route

--- express-back-end/application.js
+++ express-back-end/application.js
@@ -8,7 +8,6 @@ const app = express();
 const db = require("./database");
 
 const items = require("./routes/items");
-const newItem = require("./routes/new")
 
 module.exports = function application( ENV ) {
   app.use(cors());
@@ -16,7 +15,6 @@ module.exports = function application( ENV ) {
   app.use(bodyparser.json());
 
   app.use("/api", items(db));
-  app.use("/api", newItem(db));
 
   app.close = function() {
     return db.end();
