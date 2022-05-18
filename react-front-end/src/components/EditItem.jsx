0d81bbe894ea6b4c583924e5dd2d1ff6e2f350cb
RiazK <RiazCKhan@gmail.com>
2022-05-18 09:25:40 -0400

    destructure param

--- react-front-end/src/components/EditItem.jsx
+++ react-front-end/src/components/EditItem.jsx
@@ -1,10 +1,19 @@
-import NewItem from "./NewItem"
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import NewItem from "./NewItem"
+import axios from "axios";
 
 
 export default function EditItem() {
 
   console.log('what am i', useParams())
+
+  let { itemId } = useParams()
+  console.log(itemId)
+
+useEffect(() => {
+  // axios.get(`/api/item/`)
+})
 
   return (
     <>
