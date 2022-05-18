b5c66d604aea0bbdc0852e40da5118a09654ef65
RiazK <RiazCKhan@gmail.com>
2022-05-18 08:42:00 -0400

    create edit route page

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from './components/NavBar';
 import Home from "./components/Home";
 import NewItem from "./components/NewItem";
+import EditItem from "./components/EditItem";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<NewItem />} />
+        <Route path="/edit" element={<EditItem />} />
       </Routes>
 
     </BrowserRouter>
