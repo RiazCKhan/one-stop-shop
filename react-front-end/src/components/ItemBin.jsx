e9b3978d375fe454e673b7225dccd0552468d143
RiazK <RiazCKhan@gmail.com>
2022-05-18 16:21:51 -0400

    receive data

--- react-front-end/src/components/ItemBin.jsx
+++ react-front-end/src/components/ItemBin.jsx
@@ -1,7 +1,19 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
 export default function ItemBin() {
+const [deletedItems, setDeletedItems] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/deletedItems')
+      .then((response) => setDeletedItems(response.data))
+      .catch((error) => console.log('Error', error))
+  }, []);
+
+console.log(deletedItems)
+
   return (
     <>
-    Hello
     </>
   )
 }
