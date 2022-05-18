8f9abf64d44c6c0f25904908decf9913d91dfabe
RiazK <RiazCKhan@gmail.com>
2022-05-18 14:23:09 -0400

    add new page to nav bar

--- react-front-end/src/components/NavBar.jsx
+++ react-front-end/src/components/NavBar.jsx
@@ -4,12 +4,15 @@ import "./NavBar.css"
 export default function NavBar() {
   return (
     <section className="nav-container">
+      <h1>One Stop Shop - Admin Panel</h1>
       <Link to="/">
         <h3>All Items</h3>
       </Link>
-      <h1>One Stop Shop - Admin Panel</h1>
       <Link to="/new">
         <h3>Add Item</h3>
+      </Link>
+      <Link to="/bin">
+        <h3>Deleted Items</h3>
       </Link>
     </section>
   )
