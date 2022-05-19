ec7425f4580e5e01956decdd239098453f0c541b
RiazK <RiazCKhan@gmail.com>
2022-05-19 15:09:56 -0400

    Delete btn is now Link to new front end point

--- react-front-end/src/components/ShopListItem.jsx
+++ react-front-end/src/components/ShopListItem.jsx
@@ -2,11 +2,8 @@ import { Link } from "react-router-dom";
 import "./ShopList.css"
 
 export default function ShopListItem(props) {
-  const { id, title, description, cost,
-    onDelete, comment, setComment,
-    handleSubmit, bin,
-    deletionComment, onRestore } = props;
-
+  const { id, title, description, cost, handleSubmit,
+    onDelete, comment, setComment, bin, deletionComment, onRestore } = props;
 
   return (
     <article className="item-card">
@@ -22,28 +19,16 @@ export default function ShopListItem(props) {
           <div className="center-space">
             $ {cost}
           </div>
+
           {bin && <p className="center-space">Deletion Comment: {deletionComment}</p>}
+
           <div className="edit-input">
             {bin ? <button onClick={() => { onRestore(id) }}>Restore</button> : <Link to={`/edit/${id}`}><button>Edit</button></Link>}
-            {bin ? null : <button onClick={() => console.log('delete button')}>Delete</button>}
+            {bin ? null : <Link to={`/delete/${id}`}><button>Delete</button></Link>}
           </div>
+
         </form>
       </article>
-
-      <article>
-        <h3>Provide Deletion Comment</h3>
-        <input
-          type="text"
-          value={comment || ""}
-          onChange={(event) => {
-            setComment(event.target.value)
-          }}
-        />
-        <p>Are you sure </p>
-        <button onClick={() => onDelete(id, comment)}>Confirm</button>
-        <button onClick={() => console.log('close dialogue')}>Close</button>
-      </article>
-
     </article>
   )
 }
